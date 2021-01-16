477cdbe6b08cd906230ceaeb6b373ab2261115e0
Drakmyth <shaun.hamman@gmail.com>
2021-01-16 13:50:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Unreal Engine Marketplace Additional Asset Filters User Script
+[![License](https://img.shields.io/github/license/Drakmyth/ue-marketplace-filters)](https://github.com/Drakmyth/ue-marketplace-filters/blob/master/LICENSE.md)
+
+A Tampermonkey userscript to add additional asset filters to the Unreal Engine Marketplace
+
+## Included Filters
+
+- Hide/Show Owned Assets
+- Hide/Show External Assets
+
+Issues
+--------------------
+If you come across something that seems like a bug, please report it here. Make sure to follow these guidelines to ensure your report isn't closed as "Invalid":
+
+* Make sure you are using the latest Release version of the script. Old versions of the script are unsupported. Features and fixes will not be backported to old versions.
+* Search to see if someone else has already reported the bug. Duplicate reports just slow down getting things fixed.
+* Include steps to reproduce the issue in your report. Screenshots or videos may also prove helpful.
+
+Feature Requests
+----------------
+Have an idea for other new filters or features that would fit well in this script? Awesome! File an issue and detail your idea. Keep it succinct and on-point, and only one feature per request, or it will be closed as "Invalid". Then we'll discuss your idea some and decide whether to implement it or not.
+
+Contributions
+-------------
+This script is open source! That means if you'd like to try your hand at fixing a bug or implementing a feature, please do so! Head over to the Issues page and look for any open issues tagged with the "Accepted" label. Create a branch, work on it, then submit a pull request. We'll work together to iron out any concerns with your code, and then we'll merge it in and your code will become a part of this script's legacy!
+
+License
+-------
+ue-marketplace-filters.user.js is licensed under the MIT License (MIT). See LICENSE for details.
---
+++ ue-marketplace-filters.user.js
@@ -0,0 +1,103 @@
+// ==UserScript==
+// @name         Unreal Engine Marketplace Additional Asset Filters
+// @namespace    https://github.com/Drakmyth/ue-marketplace-filters
+// @version      0.1
+// @author       Drakmyth
+// @description  A Tampermonkey userscript to add additional asset filters to the Unreal Engine Marketplace
+// @homepage     https://github.com/Drakmyth/ue-marketplace-filters
+// @updateURL    https://github.com/Drakmyth/ue-marketplace-filters/raw/master/ue-marketplace-filters.user.js
+// @downloadURL  https://github.com/Drakmyth/ue-marketplace-filters/raw/master/ue-marketplace-filters.user.js
+// @supportURL   https://github.com/Drakmyth/ue-marketplace-filters/issues
+// @match        https://www.unrealengine.com/marketplace/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var hideOwned = false;
+    var hideExternal = false;
+
+    function doControlsExist() {
+        var sortContainer = getSortContainer();
+        return sortContainer.querySelector("span.filter-controls");
+    }
+
+    function addControls() {
+        var controlsElement = document.createElement("span");
+        controlsElement.className = "filter-controls";
+
+        var hideOwnedCheckbox = createCheckbox("Hide Owned", hideOwned, toggleHideOwned);
+        controlsElement.appendChild(hideOwnedCheckbox);
+
+        var hideExternalCheckbox = createCheckbox("Hide External", hideExternal, toggleHideExternal);
+        controlsElement.appendChild(hideExternalCheckbox);
+
+        var sortContainer = getSortContainer();
+        sortContainer.appendChild(controlsElement);
+    }
+
+    function getSortContainer() {
+        return document.getElementsByClassName('sort-select')[0];
+    }
+
+    function createCheckbox(text, initial, onChange) {
+        var labelElement = document.createElement("label");
+        labelElement.style.marginLeft = "5px";
+        labelElement.style.marginRight = "5px";
+
+        var checkboxElement = document.createElement("input");
+        checkboxElement.type = "checkbox";
+        checkboxElement.checked = initial;
+        checkboxElement.style.marginRight = "3px";
+        checkboxElement.addEventListener("change", onChange);
+        labelElement.appendChild(checkboxElement);
+
+        var textElement = document.createTextNode(text);
+        labelElement.appendChild(textElement);
+
+        return labelElement;
+    }
+
+    function toggleHideOwned(event) {
+        hideOwned = event.target.checked;
+        onBodyChange();
+    }
+
+    function toggleHideExternal(event) {
+        hideExternal = event.target.checked;
+        onBodyChange();
+    }
+
+    function isContainerOwned(container) {
+        return container.querySelector("article.asset--owned");
+    }
+
+    function isContainerExternal(container) {
+        return !container.querySelector("span.btn");
+    }
+
+    function onBodyChange(mut) {
+
+        if (!doControlsExist()) {
+            addControls();
+        }
+
+        var assetContainers = document.getElementsByClassName('asset-container');
+
+        for (let container of assetContainers) {
+            var isOwned = isContainerOwned(container);
+            var isExternal = isContainerExternal(container);
+            if ((hideOwned && isOwned) || (hideExternal && isExternal)) {
+                container.style.display = 'none';
+            } else {
+                container.style.display = null;
+            }
+        }
+    }
+
+
+    var mo = new MutationObserver(onBodyChange);
+    mo.observe(document.body, {childList: true, subtree: true});
+})();
